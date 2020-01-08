52fb04bce9ae9e2198306b24fb6fdc044d1730cb
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2020-01-08 07:49:39 -0500

    update site

--- r/cv-utils.R
+++ r/cv-utils.R
@@ -42,24 +42,25 @@ file.copy("static/rosenberg-cv.pdf", "static/cv/rosenberg-cv.pdf", overwrite=TRU
 #     reference_docx: Rosenberg-CV-template.docx
 # 
 # ## For HTML
-# file.copy("about-source.Rmd", "content/about.Rmd", overwrite=TRUE)
-# 
-# cv_html <- readLines("content/about.Rmd")
-# 
-# find_yml <- stringr::str_detect(cv_html, "---")
-# end_of_yml <- which(find_yml)[2]
-# 
-# # find_toc <- stringr::str_detect(cv_html, "render_toc")
-# # toc <- which(find_toc)
-# # cv_html <- append(cv_html, "```{r, echo = FALSE}", after = toc)
-# # cv_html <- append(cv_html, "```", after = toc-1)
-# 
-# cv_html <- append(cv_html, "PDF version: <a href = 'http://jmichaelrosenberg.com/rosenberg-cv.pdf'> <img src='/logos/pdf.png' style='width: 25px; height: 25px;'/></a>", after = end_of_yml)
-# 
-# cv_html <- cv_html[cv_html!="\\begin{center}"]
-# cv_html <- cv_html[cv_html!="\\end{center}"]
-# cv_html[stringr::str_detect(cv_html, "Curriculum Vitae")] <- "*Curriculum Vitae*"
-# cv_html <- cv_html[cv_html!="\\begingroup"]
-# cv_html <- cv_html[cv_html!="\\hangindent=2em"]
-# cv_html <- cv_html[cv_html!="\\center"]
-# cv_html <- cv_html[cv_html!="\\endgroup"]
+file.copy("about-source.Rmd", "content/about.Rmd", overwrite=TRUE)
+
+cv_html <- readLines("content/about.Rmd")
+
+find_yml <- stringr::str_detect(cv_html, "---")
+end_of_yml <- which(find_yml)[2]
+
+# find_toc <- stringr::str_detect(cv_html, "render_toc")
+# toc <- which(find_toc)
+# cv_html <- append(cv_html, "```{r, echo = FALSE}", after = toc)
+# cv_html <- append(cv_html, "```", after = toc-1)
+
+cv_html <- append(cv_html, "PDF version: <a href = 'http://jmichaelrosenberg.com/rosenberg-cv.pdf'> <img src='/logos/pdf.png' style='width: 25px; height: 25px;'/></a>", after = end_of_yml)
+
+cv_html <- cv_html[cv_html!="\\begin{center}"]
+cv_html <- cv_html[cv_html!="\\end{center}"]
+cv_html[stringr::str_detect(cv_html, "Curriculum Vitae")] <- "*Curriculum Vitae*"
+cv_html <- cv_html[cv_html!="\\begingroup"]
+cv_html <- cv_html[cv_html!="\\hangindent=2em"]
+cv_html <- cv_html[cv_html!="\\center"]
+cv_html <- cv_html[cv_html!="\\endgroup"]
+readr::write_lines(unlist(cv_html), "content/about.Rmd")
